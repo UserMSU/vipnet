2f75e0ab6357f6a0faba887467fa02db2cc66ac8
UserMSU <149796331+UserMSU@users.noreply.github.com>
2024-10-16 12:33:57 +0300

    Update 1vipnet.sh

--- 1vipnet.sh
+++ 1vipnet.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/expect -f
+LOG='user'
+VIP='192.168.16.10'
 set timeout 1
 spawn ./ip_vipnet.sh
 sleep 10 # таймайт можно ставить меньше, в зависимости от производительности VIPNET'а. Например, к HW50 долго подключаться по SSH.
