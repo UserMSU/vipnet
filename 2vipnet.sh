4d564836b867b8dcdd8f35d4abc12cfdc130ab5c
UserMSU <149796331+UserMSU@users.noreply.github.com>
2024-10-16 12:31:54 +0300

    Create 2vipnet.sh

---
+++ 2vipnet.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/expect -f
+LOG='user'
+VIP='192.168.16.10'
+set timeout 1
+spawn ./ip_vipnet.sh
+sleep 5
+expect "$LOG@$VIP's password:\r"
+send -- "machine halt"
+sleep 60
+spawn ./ip_vipnet.sh
+sleep 5
+expect "$LOG@$VIP's password:\r"
+send -- "machine halt"
+expect eof
